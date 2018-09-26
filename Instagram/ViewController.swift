386ea5fdff97cc59c21ed3286d75476b470c94cf
Jesus Andres Bernal Lopez <jabernall24@gmail.com>
2018-09-25 22:20:10 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Jesus Andres Bernal Lopez on 9/25/18.
+//  Copyright © 2018 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
